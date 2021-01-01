4d0c591d0316574182c0be0376d04f28fc6c8f1c
Darnell Baird <drsbaird@yahoo.com>
2021-01-01 16:15:59 -0400

    Created a steady Kinetic Particle. no unit tests done

--- lib.py
+++ lib.py
@@ -23,6 +23,8 @@ class RoamingController():
 
 class KineticParticle():
     dimensions = 1
+    #calculation range
+    average_between = 2
 
     time = time.monotonic()
     position = 0
@@ -34,8 +36,52 @@ class KineticParticle():
     velocities = []
     accelerations = []
 
-    def __init__(self):
+    def __init__(self, name=''):
         self.time = time.monotonic()
+
+    def __getattr__(self, name):
+        if name == 'pos':
+            return self.position
+        elif name == 'vel':
+            return self.velocity
+        elif name == 'accel':
+            return self.acceleration
+        elif name in ['vels', 'velocitys']
+            return self.velocities
+        elif name == 'accels':
+            return self.acceleartions
+
+    def valid(self, key, value):
+        #validify the value according to its field
+        booly = True
+        if key in ['pos','position','vel','velocity','accel','acceleration','time', ]:
+            booly &= isinstance(value, (int,float))
+            if key == 'time':
+                booly &= value >= 0
+        if key in ['positions','velocities','velocitys','accelerations','times']:
+            booly &= isinstance(value, list)
+            try:
+                booly &= all([isinstance(x, (int,float) for x in value)])
+                if key == 'times':
+                    booly &= all([x>=0 for x in value])
+            except Attribute, ValueError as e:
+                return False
+        return booly
+
+    def last_value(self, key, amount=1, force=True):
+        #get the last 'amount' values for key
+        to_return = []
+        key += 's'  #plural key
+        vals = getattr(self,key)
+        for i in range(amount):
+            try:
+                val = vals[-i]
+                to_return.append(val)
+            except AttributeError, ValueError as e:
+                logging.critical("Cannot get key '{}' at index '{}', {}".format(key,i,e))
+                if not force:
+                    return []
+        return to_return
 
     def clear(self):
         if self.dimension == 1:
@@ -43,9 +89,33 @@ class KineticParticle():
             self.position, self.velocity, self.acceleartion  = 0, 0, 0
             self.times, self.positions, self.velocities, self.accelerations = [], [], [], []
 
-    def calculate(self):
-        pass
-
-    def update(self, position=self.position, velocity=self.velocity, acceleration=self.acceleration):
-        pass
+    def update(self, time=self.time, pos=self.position, vel=self.velocity, accel=self.acceleration):
+        #puts active values to stored values. completes any calculations required
+        #must have position. for every consecutive field, either get it or calculate
+        if self.valid('pos',pos):
+            #velocity
+            if not self.valid('vel',vel):
+                last_pos = self.last_value('vel')
+                last_time = self.last_value('time')
+                if last_pos and last_time:
+                    vel = (pos-last_pos)/(time-last_time)
+            if self.valid('vel',vel):
+                #acceleration
+                if not self.valid('accel',accel):
+                    last_vel = self.last_value('accel')
+                    last_time = self.last_value('time')
+                    if last_vel and last_time:
+                        accel = (vel-last_vel)/(time-last_time)
+            else:
+                logging.critical("Cannot update {} as velocity '{}' is invalid.".format(self.name, vel))
+        else:
+            logging.critical("Cannot update {} as position '{}' is invalid.".format(self.name, pos))
+        #IF ALL VALID, ADD
+        if all([self.valid(k,v) for k,v in list(zip(['pos','vel','accel'],[pos,vel,accel])) ]):
+            self.positions.append(pos)
+            self.velocities.append(vel)
+            self.accelerations.append(accel)
+        else:
+            logging.critical("Could not add active units.".format())
+            
 
